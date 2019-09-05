b36f5a29a645d554a48cdaa3a174b722c5a30c3f
Aleksander Majda <nazgut@gmail.com>
2019-09-05 21:40:17 +0200

    first click stats

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-
+$(document).ready(function() {
+	var player = [];
+	var resources = {
+						'gold':{'count':0, 'show':true},
+						'food':{'count':0, 'show':true},
+						'water':{'count':0, 'show':true},
+						'medical':{'count':0, 'show':false},
+						'weapon':{'count':0, 'show':false},
+						'ammo':{'count':0, 'show':true},
+						'wood':{'count':0, 'show':true},
+						'stone':{'count':0, 'show':true},
+						'iron':{'count':0, 'show':true}
+					};
+	function init() {
+		$("#stats").html("");
+		$.each( resources, function( i, val ) {
+			if (val.show==true) {
+				$("#stats").append(i+': <span id="res_'+i+'">'+val.count+'</span><br/>');
+			}
+		});
+	}
+	
+	$( "#garbage" ).click(function() {
+		resources.gold.count ++;
+	});
+	
+	init();
+	window.setInterval(function(){init()}, 60);
+});
