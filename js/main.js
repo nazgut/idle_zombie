0320d10b942fecf2765b22bd1382fc994a456d0c
Aleksander Majda <nazgut@gmail.com>
2019-09-09 22:58:05 +0200

    add msg box and save logs

--- js/main.js
+++ js/main.js
@@ -23,15 +23,18 @@ $(document).ready(function() {
 	Game.saveFile = function(){
 		var file = {
 			food: resources.food.count,
-			water: resources.water.count
+			water: resources.water.count,
+			msg: $("#logs").html()
 		};
 		localStorage.setItem('saveFile',JSON.stringify(file));
+		Game.msg('Game saved');
 	};
 
 	Game.loadFile = function(){
 		var file = JSON.parse(localStorage.getItem('saveFile'));
 		resources.food.count = file.food;
 		resources.water.count = file.water;
+		$("#logs").html(file.msg);
 	};
 	
 	Game.getRndInteger = function(min, max) {
@@ -47,13 +50,26 @@ $(document).ready(function() {
 		});
 	};
 	
+	Game.msg = function(text) {
+		$("#logs").prepend('<p>'+text+'</p>');
+		console.log($("#logs > p").length);
+		if ($("#logs > p").length>10) $("#logs p").last().remove();
+	}
+	
 	$( "#garbage" ).click(function() {
 		$(this).progressTimed(1);
 		$(this).on('progress-finish', function() {
-			if (Game.getRndInteger(1,6) < 4) resources.food.count ++;
-			else resources.water.count ++;
-			
+			var msg = '';
+			if (Game.getRndInteger(1,6) < 4) {
+				resources.food.count ++;
+				msg = "1 food";
+			}
+			else {
+				resources.water.count ++;
+				msg = "1 water";
+			}
 			$(this).off('progress-finish');
+			Game.msg('You found '+msg);
 		});
 	});
 	
